673370e2f9c51df86eeb43339ad6a122fa78a81b
Matheus <178436024+maxmati1@users.noreply.github.com.>
2026-03-22 22:11:03 -0300

    Adiciona repository Fornecedor

---
+++ src/main/java/com/bn/ex7/repositories/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package com.bn.ex7.repositories;
+
+import com.bn.ex7.models.FornecedorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<FornecedorModel, Long> {
+}

d3cfb8017b0becced1d5978affdf6d0719d20a17
Matheus <178436024+maxmati1@users.noreply.github.com.>
2026-03-22 22:10:28 -0300

    Adiciona entidade FornecedorModel

---
+++ src/main/java/com/bn/ex7/models/FornecedorModel.java
@@ -0,0 +1,50 @@
+package com.bn.ex7.models;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Fornecedor")
+@Table(name = "tb_fornecedor")
+public class FornecedorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_fantasia")
+    private String nomeFantasia;
+
+    @Column(name = "cnpj_fornecedor")
+    private String cnpj;
+
+    private String contato;
+
+    public FornecedorModel() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
